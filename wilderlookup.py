808c8e894efaf27461f9ccc8db3fa4d88740ef2f
sma11 <36231823+lanecl1@users.noreply.github.com>
2021-12-22 23:51:04 -0600

    v3

--- wilderlookup.py
+++ wilderlookup.py
@@ -76,10 +76,10 @@ if endpoint == 'Media Lookup':
     st.markdown(link_512, unsafe_allow_html=True)
     st.markdown(link_1024, unsafe_allow_html=True)
     st.markdown(link_animation, unsafe_allow_html=True)
-
+    step=0
     While token_content:
-        st.write(token_content['data']['items'][0]['nft_data'][0]['external_data'][attribute][trait_type]& ":" & token_content['data']['items'][0]['nft_data'][0]['external_data'][attribute][value]
-    
+        st.write(f'token_content['data']['items'][0]['nft_data'][0]['external_data'][attribute]{step}[trait_type]'& ":" & 'token_content['data']['items'][0]['nft_data'][0]['external_data'][attribute]{step}[value]
+        step=step+1
    
 
 
