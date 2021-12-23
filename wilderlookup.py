a7b8ec563cc24a82ba9f7b283ba344b5e926bc70
sma11 <36231823+lanecl1@users.noreply.github.com>
2021-12-23 00:23:09 -0600

    v3

--- wilderlookup.py
+++ wilderlookup.py
@@ -52,7 +52,8 @@ if endpoint == 'Media Lookup':
     params['limit']=50
     r=requests.get(f'https://api.covalenthq.com/v1/1/tokens/0xc2e9678A71e50E5AEd036e00e9c5caeb1aC5987D/nft_metadata/{token}/?quote-currency=USD&format=JSON&key=ckey_fa91923a9dc34181ac2bbbdc82e')
     token_content=r.json()
-    st.write(token_content[data][items][0][nft_data][0][external_data][attribute])
+    st.write(token_content)
+    st.write(token_content/data/items/0/nft_data/0/external_data/attribute)
 
     st.write(token_content['data']['items'][0]['nft_data'][0]['external_data']['name'])
     st.image(token_content['data']['items'][0]['nft_data'][0]['external_data']['image_256'])
