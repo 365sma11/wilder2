de7041c34d4f0aad989d159fc346a7cf25411be9
sma11 <36231823+lanecl1@users.noreply.github.com>
2021-12-23 00:32:11 -0600

    v4

--- wilderlookup.py
+++ wilderlookup.py
@@ -53,7 +53,7 @@ if endpoint == 'Media Lookup':
     r=requests.get(f'https://api.covalenthq.com/v1/1/tokens/0xc2e9678A71e50E5AEd036e00e9c5caeb1aC5987D/nft_metadata/{token}/?quote-currency=USD&format=JSON&key=ckey_fa91923a9dc34181ac2bbbdc82e')
     token_content=r.json()
     st.write(token_content)
-    st.write(token_content['data']['items'][0]['nft_data'][0]['external_data']['attributes'])
+
 
     st.write(token_content['data']['items'][0]['nft_data'][0]['external_data']['name'])
     st.image(token_content['data']['items'][0]['nft_data'][0]['external_data']['image_256'])
@@ -77,9 +77,9 @@ if endpoint == 'Media Lookup':
     st.markdown(link_512, unsafe_allow_html=True)
     st.markdown(link_1024, unsafe_allow_html=True)
     st.markdown(link_animation, unsafe_allow_html=True)
-    step=0
 
-    df= pd.dataframe(token_content['data']['items'][0]['nft_data'][0]['external_data']['attributes'])
+    st.write(token_content['data']['items'][0]['nft_data'][0]['external_data']['attributes'])
+    df=token_content['data']['items'][0]['nft_data'][0]['external_data']['attributes']
     st.dataframe(df)
    
 
