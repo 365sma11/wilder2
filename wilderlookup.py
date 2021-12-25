ae34276ca23085d40b97b41b1355eeaf7bfd1f0d
sma11 <36231823+lanecl1@users.noreply.github.com>
2021-12-25 01:08:19 -0600

    1.18
    Kicks added
    -need to extract name & attributes

--- wilderlookup.py
+++ wilderlookup.py
@@ -92,11 +92,11 @@ elif endpoint == 'Kicks':
         api_key='ckey_fa91923a9dc34181ac2bbbdc82e'  # Get your api key here: https://www.covalenthq.com/platform/#/apikey/
         r=requests.get(f'https://api.covalenthq.com/v1/1/tokens/0xc2e9678A71e50E5AEd036e00e9c5caeb1aC5987D/nft_metadata/{token}/?quote-currency=USD&format=JSON&key={api_key}')
         token_content=r.json()
-        st.write(token_content)
+        #st.write(token_content)
 
         # Write token content
         #st.write(token_content['data']['items'][0]['nft_data'][0]['external_data']['name'])
-        st.image(token_content['data']['items'][0]['nft_data'][0]['external_data']['image_512'])
+        #st.image(token_content['data']['items'][0]['nft_data'][0]['external_data']['image_512'])
         description = token_content['data']['items'][0]['nft_data'][0]['external_data']['description']
         image256 = token_content['data']['items'][0]['nft_data'][0]['external_data']['image_256']
         image512 = token_content['data']['items'][0]['nft_data'][0]['external_data']['image_512']
